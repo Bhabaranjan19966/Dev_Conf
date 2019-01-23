e79c3b825921af59a32de814617031231d6c34ab
Aggidevaraswetha <swetha.aggi123@gmail.com>
2019-01-23 17:56:33 +0530

    changed loader

--- src/components/barGraph.js
+++ src/components/barGraph.js
@@ -1,10 +1,12 @@
-import React from  'react';
+import React, { Fragment } from  'react';
 import {BarChart} from 'react-d3-components';
+import {Loader, Segment} from 'semantic-ui-react'
 
 class BarGraph extends React.Component{
     constructor(props){
         super(props);
         this.state={
+            showLoader:true,
             data :[{
                 label: 'somethingA',
                 values: [{x: 'SomgA', y: 10}, {x: 'SomethingB', y: 4.5}, {x: 'SomethingC', y: 3}]
@@ -12,7 +14,7 @@ class BarGraph extends React.Component{
         }
 
         this.networkCallHandler = this.networkCallHandler.bind(this);
-
+        
     }
 
     networkCallHandler(){
@@ -23,17 +25,36 @@ class BarGraph extends React.Component{
         })
     }
 
+    renderGrpah =()=>{
+        setTimeout(()=>{
+            console.log("setting the state");
+            this.setState({
+                showLoader:false
+            })
+        },2000)
+    }
+
+
     render(){
 
         return (
-        <div>
-        <h3 style={{color:"grey", fontFamily:"sans", marginLeft:"18vw"}}> Idea vs ratings</h3>
-        <BarChart
-        data={this.state.data}
-        width={400}
-        height={400}
-        margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
-        </div>
+            <Fragment>
+            { this.state.showLoader ? 
+              <Segment style={{width:"inherit",height:"20vh"}} >
+              {this.renderGrpah()}
+              <Loader content="Preparing Graphs for you" active />
+            </Segment>
+                 : 
+          (  <div>
+            <h3 style={{color:"grey", fontFamily:"sans", marginLeft:"18vw"}}> Idea vs ratings</h3>
+            <BarChart
+            data={this.state.data}
+            width={400}
+            height={400}
+            margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
+            </div>)
+        }
+        </Fragment>
         );
     }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -11,24 +11,173 @@ export default class Graph extends Component {
       options: [
         {
           id: "1",
-          value:
-            "import SyntaxHighlighter from 'react-syntax-highlighter';\nimport { docco } from 'react-syntax-highlighter/dist/styles/hljs';\nconst Component = () => {\nconst codeString = '(num) => num + 1';\nreturn <SyntaxHighlighter language='javascript' style={docco}>{codeString}</SyntaxHighlighter>;  \n}",
+          value: `experiment_name: Example1_Iris_Classification
+          owner: sunbird
+            
+          inputs:
+            raw_data: "inputs/iris.csv"
+          outputs:
+            report: 'outputs/model_report'
+            model: 'outputs/model'
+          
+          graph:
+          
+            - node_name: split
+              inputs: raw_data
+              outputs: [train, test]
+              operation: sklearn.Splitters
+              arguments:
+                model_args:
+                  name: train_test_split
+                  arguments:
+                    test_size: 0.2
+              imports:
+                - [sklearn.model_selection, train_test_split]
+          
+            - node_name: model_logistic_regression
+              inputs: split.train
+              outputs: [report, model]
+              operation: sklearn.CrossValidate
+              arguments:
+                model_args:
+                  name: LogisticRegression
+                  arguments:
+                    max_iter: 200
+                target_variable: Species
+                cv_args:
+                  cv: 5
+              imports:
+                - [sklearn.linear_model, LogisticRegression]`,
           text: "Java"
         },
         {
           id: "2",
-          value:
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.\n The point of using \nLorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \n'Content here, content here', making it look like readable English.\n Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, \nand a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, \nsometimes on purpose (injected humour and the like)",
+          value: `experiment_name: Content_tagging
+          owner: sunbird
+          
+          inputs:
+            DS_DATA_HOME: /home/DS_DATA_HOME
+            pathTotaxonomy: inputs/RevisedTaxonomy.csv
+            categoryLookup: inputs/category_lookup.yaml
+            pathTotriggerJson: inputs/Trigger_node_ip.json
+            pathTocorpus: inputs/corpus
+          
+          outputs:
+            dummy: /home/DS_DATA_HOME
+          
+          graph:
+            - node_name: contentmeta_creation
+              inputs: [DS_DATA_HOME, pathTotriggerJson]
+              outputs: [pathTocontentMeta]
+              operation: contentTagging.ContentmetaCreation
+              arguments:
+                copy_to: /home/DS_DATA_HOME
+                file_name: contentMeta
+          
+            - node_name: content_to_text
+              inputs: [contentmeta_creation.pathTocontentMeta]
+              outputs: [timestamp_folder]
+              operation: contentTagging.ContentToText
+              arguments:
+                range_start: 4
+                range_end: 10
+                num_of_processes: 1
+                subset_contentMeta_by: youtube
+                content_type:
+                  youtube:
+                    contentDownloadField: artifactUrl
+                    video_to_speech: none
+                    speech_to_text: googleAT
+                    image_to_text: none
+                    pdf_to_text: none
+                    ecml_index_to_text: none
+                  ecml:
+                    contentDownloadField: downloadUrl
+                    video_to_speech: ffmpeg
+                    speech_to_text: googleAT
+                    image_to_text: googleVision
+                    pdf_to_text: PyPDF2
+                    ecml_index_to_text: parse
+                  pdf:
+                    contentDownloadField: downloadUrl
+                    video_to_speech: none
+                    speech_to_text: none
+                    image_to_text: none
+                    pdf_to_text: PyPDF2
+                    ecml_index_to_text: none
+          
+            - node_name: keyword_extraction
+              inputs: [pathTotaxonomy, categoryLookup, content_to_text.timestamp_folder, pathTocorpus]
+              outputs: [pathTocontentKeywords]
+              operation: contentTagging.KeywordExtraction 
+              arguments: 
+                extract_keywords: tagme
+                filter_criteria: none
+                update_corpus: 0
+                filter_score_val: 0.4
+                num_keywords: 5
+          
+            - node_name: write_to_elastic_search
+              inputs: [content_to_text.timestamp_folder]
+              outputs: []
+              operation: contentTagging.WriteToElasticSearch
+          
+            - node_name: corpus_creation
+              inputs: [pathTotaxonomy, keyword_extraction.pathTocontentKeywords]
+              outputs: [root_path, path_to_corpus]
+              operation: contentTagging.CorpusCreation
+              arguments:
+                keyword_folder_name: tagme_none
+                update_corpus: True
+                word_preprocess: 
+                   delimitter: "_"
+                   method: stem_lem
+          
+            - node_name: content_taxonomy_scoring
+              inputs: [contentmeta_creation.pathTocontentMeta, pathTotaxonomy, corpus_creation.root_path, corpus_creation.path_to_corpus]
+              outputs: [path_to_timestampFolder, path_to_distMeasure, path_to_domain_level]
+              operation: contentTagging.ContentTaxonomyScoring
+              arguments:
+                keyword_extract_filter_by: tagme_none
+                phrase_split: True
+                min_words: 5
+                distanceMeasure: jaccard
+                embedding_method: none
+                delimitter: "_"
+                filter_by:
+                  contentMeta:
+                    column: subject
+                    alignment_depth: none
+                  taxonomy:
+                    column: Subject
+                    alignment_depth: Chapter Name
+          
+            - node_name: predict_tag
+              inputs: [content_taxonomy_scoring.path_to_timestampFolder]
+              outputs: [path_to_predictedTags]
+              operation: contentTagging.PredictTag
+              arguments:
+                window: 5
+          
+            - node_name: generate_observed_tag
+              inputs: [contentmeta_creation.pathTocontentMeta, pathTotaxonomy, content_taxonomy_scoring.path_to_timestampFolder]
+              outputs: [path_to_timestampFolder, path_to_observedtag, path_to_predictedtag]
+              operation: contentTagging.GenerateObservedTag
+              arguments:
+                window: 5
+                level: Chapter Name
+                tax_known_tag: Grade
+                content_known_tag: gradeLevel
+          
+            - node_name: evaluation
+              inputs: [generate_observed_tag.path_to_timestampFolder, generate_observed_tag.path_to_observedtag, generate_observed_tag.path_to_predictedtag]
+              outputs: [path_to_agg_precision, path_to_nonagg_precision]
+              operation: contentTagging.Evaluation
+              arguments:
+                window: 5`,
           text: "Javascript"
         },
-        {
-          id: "3",
-          value:
-            "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.\n If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. \nAll the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, \nmaking this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, \n combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. \n The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-          text: "HTML"
-        }
       ],
-      selected: ["1"],
       displayGraph: false
     });
   }
@@ -49,7 +198,7 @@ export default class Graph extends Component {
         displayLoader: false,
         displayGraph: true
       });
-    }, 3000);
+    }, 0);
   };
   render() {
     return (
@@ -69,21 +218,26 @@ export default class Graph extends Component {
           </Card>
 
           {this.state.displayContent ? (
-            <div class="ui container">
-              <SyntaxHighlighter
+            <Fragment>            <div className="cont-style">
+            <div className= "code-container"> 
+              <SyntaxHighlighter className="data"
                 language="javascript"
                 showLineNumbers="true"
                 style={docco}
               >
                 {this.state.content}
               </SyntaxHighlighter>
-              <button
-                className="ui button raised primary right floated"
-                onClick={this.displayGraph}
-              >
-                Deploy
-              </button>
+         
             </div>
+            </div>
+                 <button
+                 className="ui button raised primary right floated deploy-btn"
+                 onClick={this.displayGraph}
+               >
+                 Deploy
+               </button>
+               </Fragment>
+
           ) : (
             ""
           )}
@@ -92,16 +246,7 @@ export default class Graph extends Component {
             <div className="bargraph">
             <DisplayGraph/>
             </div>
-          ) : this.state.displayLoader === true ? (
-            <div class="loader-bg">
-              <div class="ui active inverted dimmer">
-                <div class="ui medium text loader">
-                  Preparing your graphs for you.
-                </div>
-              </div>
-              <p />
-            </div>
-          ) : (
+          ) :(
             ""
           )}
         </Container>
